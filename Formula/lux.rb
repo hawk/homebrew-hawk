79e01659a9196f647de94d71e37e96803e4079c0
Håkan Mattsson <hmattsso@cisco.com>
2019-04-03 13:16:41 +0200

    Upgrade Homebrew formula to lux 2.0

--- Formula/lux.rb
+++ Formula/lux.rb
@@ -1,37 +1,23 @@
 class Lux < Formula
-  desc "Lux (LUcid eXpect scripting) simplifies test automation and provides an Expect-style execution of commands"
+  desc "Test automation framework with Expect style execution of commands"
   homepage "https://github.com/hawk/lux"
-  url "https://github.com/hawk/lux/archive/lux-1.19.3.tar.gz"
-  sha256 "e2dd36275c76a087a4b4577bd056a6928a5e5ba5b00d8c366e2a4f94b16f921e"
+  url "https://github.com/hawk/lux/archive/lux-2.0.tar.gz"
+  sha256 "7199de8b970932206246b8700616f0d3077d51e6f574c5c646a78c07a96ad65d"
 
-  head do
-    url "https://github.com/hawk/lux.git"
-  end
+  head "https://github.com/hawk/lux.git"
 
   depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-
-  option "without-runpty", "Omit pseudo TTY wrapper"
-
   depends_on "erlang"
-  depends_on "wxmac" => :optional
 
   def install
-    ENV.deparallelize  # if your formula fails when building in parallel
-
     system "autoconf"
-    system "./configure", "--prefix=#{prefix}/lux"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
-
-    #system "make", "install"
-    system "mkdir -p #{prefix}/lux"
-    system "tar cf - * | (cd #{prefix}/lux && tar xf -)"
-    system "mkdir -p #{prefix}/bin"
-    system "ln -s #{prefix}/lux/bin/lux #{prefix}/bin/lux"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/lux", "--version"
+    output = shell_output("bin/lux examples/calc.lux")
+    assert_match "SUCCESS", output
   end
 end
