6cb256f657a82cd39436fb5eedd4803b137b5048
Håkan Mattsson <hmattsso@cisco.com>
2019-04-07 13:51:25 +0200

    Upgrade Homebrew formula to lux 2.0.2

--- Formula/lux.rb
+++ Formula/lux.rb
@@ -1,8 +1,8 @@
 class Lux < Formula
   desc "Test automation framework with Expect style execution of commands"
   homepage "https://github.com/hawk/lux"
-  url "https://github.com/hawk/lux/archive/lux-2.0.1.tar.gz"
-  sha256 "8d21aad2bf443f535c0caa5a83b20e5c57a45a39ecb78058c6988d5fde2addbe"
+  url "https://github.com/hawk/lux/archive/lux-2.0.2.tar.gz"
+  sha256 "9fddc54d6999ae4bf9180df514b65fda998c39a5bad3de4386d3ae834a0b9f23"
 
   head "https://github.com/hawk/lux.git", :branch => "develop"
 
@@ -17,7 +17,15 @@ class Lux < Formula
   end
 
   test do
-    output = shell_output("bin/lux examples/calc.lux")
+   (testpath/"test.lux").write <<~EOS
+    [doc Simple test]
+    [shell myshell]
+        !echo foo
+        ?echo foo
+        ?^foo
+    EOS
+
+    output = shell_output("lux test.lux")
     assert_match "SUCCESS", output
   end
 end
