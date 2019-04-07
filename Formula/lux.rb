ea6bc38f1fea921f094e2ea6e2d0011d3f0d3d19
Håkan Mattsson <hmattsso@cisco.com>
2019-04-07 13:21:29 +0200

    Upgrade Homebrew formula to lux 2.0.1

--- Formula/lux.rb
+++ Formula/lux.rb
@@ -4,7 +4,7 @@ class Lux < Formula
   url "https://github.com/hawk/lux/archive/lux-2.0.1.tar.gz"
   sha256 "8d21aad2bf443f535c0caa5a83b20e5c57a45a39ecb78058c6988d5fde2addbe"
 
-  head "https://github.com/hawk/lux.git"
+  head "https://github.com/hawk/lux.git", :branch => "develop"
 
   depends_on "autoconf" => :build
   depends_on "erlang"
