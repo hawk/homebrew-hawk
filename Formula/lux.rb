1635414dccfe109a0844a84ff1edd05559f04a81
Håkan Mattsson <hmattsso@cisco.com>
2019-05-15 13:25:15 +0200

    Upgrade Homebrew formula to lux 2.1

--- Formula/lux.rb
+++ Formula/lux.rb
@@ -1,8 +1,8 @@
 class Lux < Formula
   desc "Test automation framework with Expect style execution of commands"
   homepage "https://github.com/hawk/lux"
-  url "https://github.com/hawk/lux/archive/lux-2.0.2.tar.gz"
-  sha256 "9fddc54d6999ae4bf9180df514b65fda998c39a5bad3de4386d3ae834a0b9f23"
+  url "https://github.com/hawk/lux/archive/lux-2.1.tar.gz"
+  sha256 "ba7282217f9a88a59a27656e78ac6c52909b4e73bc2d31fd7f62543f71993a26"
 
   head "https://github.com/hawk/lux.git", :branch => "develop"
 
